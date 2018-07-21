f379dd167f23849e33f477f547de9920d5fbbe85
alex <alex@pdp7.net>
2018-07-21 21:41:56 +0200

    Add media source list view

--- pyccw/main/views.py
+++ pyccw/main/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from pyccw.main import models
+
+
+class MediaSourceListView(generic.ListView):
+    model = models.MediaSource
--- pyccw/urls.py
+++ pyccw/urls.py
@@ -1,3 +1,5 @@
+from pyccw.main import views
+
 """pyccw URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,4 +20,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.MediaSourceListView.as_view()),
 ]
