c84666cf615067f74486e6a02f1bda26aeab2b6e
alex <alex@pdp7.net>
2018-07-21 21:51:40 +0200

    Hook mediasource_path view

--- pyccw/main/views.py
+++ pyccw/main/views.py
@@ -1,3 +1,4 @@
+from django import shortcuts
 from django.views import generic
 
 from pyccw.main import models
@@ -5,3 +6,8 @@ from pyccw.main import models
 
 class MediaSourceListView(generic.ListView):
     model = models.MediaSource
+
+
+def mediasource_path(request, mediasource, path):
+    ms = shortcuts.get_object_or_404(models.MediaSource, name=mediasource)
+    return shortcuts.render(request, 'main/mediasource_path.html', {'media_source': ms, 'path': path})
--- pyccw/urls.py
+++ pyccw/urls.py
@@ -21,4 +21,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.MediaSourceListView.as_view()),
+    path('media_source/<str:mediasource>/<path:path>', views.mediasource_path, name='mediasource_path')
 ]
