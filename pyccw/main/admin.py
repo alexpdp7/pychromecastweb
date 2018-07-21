c1df4d7e2653b524420a919ed81af9ac584d1fab
alex <alex@pdp7.net>
2018-07-21 21:31:14 +0200

    Add MediaSource

--- pyccw/main/admin.py
+++ pyccw/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from pyccw.main import models
+
+
+admin.site.register(models.MediaSource)
--- pyccw/main/models.py
+++ pyccw/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class MediaSource(models.Model):
+    name = models.CharField(max_length=1000)
+    path = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
