87b0992d29c335431f91084ae7b086b9fe4a6872
alex <alex@pdp7.net>
2018-08-19 12:17:31 +0200

    Add whitenoise and gunicorn as dependencies
    Informal tests show that serving media with both allows the Chromecast to seek

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup, find_packages
 
 setup(name='pychromecastweb',
       packages=find_packages(),
-      install_requires=['pychromecast','Django'],
+      install_requires=['pychromecast','Django', 'whitenoise==4.0b5', 'gunicorn'],
       extras_require={
         'dev': ['ipython', 'ipdb',],
       },
       python_requires='>=3',
 )
-      
