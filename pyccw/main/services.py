062ca4c0617e304793e94fe8893a6534bdecdb08
alex <alex@pdp7.net>
2018-08-20 10:38:03 +0200

    Fix #8: py35 requires additional str conversion

--- pyccw/main/services.py
+++ pyccw/main/services.py
@@ -61,7 +61,7 @@ def get_hostname():
 
 
 def spawn_server(path, file):
-    s = subprocess.Popen(['pyccw_serve', path, '0'], stdout=subprocess.PIPE)
+    s = subprocess.Popen(['pyccw_serve', str(path), '0'], stdout=subprocess.PIPE)
     while True:
         try:
             out = subprocess.check_output(['lsof', '-P', '-a', '-p', str(s.pid), '-i', 'TCP', '-sTCP:LISTEN'])
